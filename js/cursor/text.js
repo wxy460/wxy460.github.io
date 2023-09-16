39335962652f2ca67caa8f6d0cee3a71d8140f58
Schwarzschild114514 <734079041@qq.com>
2023-09-16 20:58:20 +0800

    Site updated: 2023-09-16 20:58:19

--- js/cursor/text.js
+++ js/cursor/text.js
@@ -1,10 +1,10 @@
 var a_idx = 0;
 jQuery(document).ready(function($) {
   $("body").click(function(e) {
-    var a = new Array("Ciallo", "Nanami");
+    var a = new Array("喜欢我", "不喜欢我");
     var $i = $("<span/>").text(a[a_idx]);
     var x = e.pageX,
-      y = e.pageY;
+    y = e.pageY;
     $i.css({
       "z-index": 99999,
       "top": y - 28,
